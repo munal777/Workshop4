daa1ef23040ca98ba538c4f178ff81f378a3c482
munal777 <munalpoudel3@gmail.com>
2025-11-18 09:34:01 +0545

    feat: completion of Task2 App

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Calculator obj = new Calculator();
+
+        int sum = obj.Add(2,3);
+        int mul = obj.Multiply(5);
+
+        obj.PrintWelcome();
+        Console.WriteLine($"sum result: {sum}, mul result: {mul}");
+    }
+}
--- Task2/obj/Debug/net9.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net9.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4fc198699345c9c7f4c56d81a271f84e743c0f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02cea81f02fdc63ffe979528a9b4847567a29647")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
