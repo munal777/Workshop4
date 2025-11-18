b80014faa8e4404b02fa819be53bcfebe2eaf9c3
munal777 <munalpoudel3@gmail.com>
2025-11-18 09:09:53 +0545

    feat: create the student class program

---
+++ Task1/Student.cs
@@ -0,0 +1,15 @@
+class Student
+{
+    string firstname;
+    string lastname;
+    int age;
+
+    static string CollageName = "IIC";
+
+    public Student(string firstname, string lastname, int age)
+    {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+    }
+}
